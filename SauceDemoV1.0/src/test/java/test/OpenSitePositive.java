dc3a83cbefc86b25fda14e0c36d4a0132ed4c82d
Ahmed Elkholy <72422338+ashawki86@users.noreply.github.com>
2024-06-08 15:12:44 +0300

    Add files via upload
    The First Automation Selenium  Code for Automating Sauce Demo website 
    My first Try

---
+++ SauceDemoV1.0/src/test/java/test/OpenSitePositive.java
@@ -0,0 +1,60 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class OpenSitePositive {
+    public static void main(String[] args) throws InterruptedException {
+        //initialize Chrome Driver
+        WebDriver driver =new ChromeDriver();
+        //Maximize Chrome Driver
+        driver.manage().window().maximize();
+        //open site
+        driver.get("https://www.saucedemo.com/");
+        //insert username and password
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        //login into products page
+        driver.findElement(By.id("login-button")).click();
+        //wait
+        Thread.sleep(3000);
+        //Add and Remove from cart
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();   //Add to Cart
+        driver.findElement(By.id("remove-sauce-labs-backpack")).click(); //remove from cart
+
+        Thread.sleep(2000); //Wait after Removing
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();   //Add to Cart Again
+        driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt")).click();   //Add to Cart Again
+        Thread.sleep(2000); //Wait
+
+        //Go to Cart Page
+        driver.findElement(By.className("shopping_cart_link")).click();
+        Thread.sleep(2000); //Wait
+        driver.findElement(By.id("checkout")).click();
+
+        //Checkout: Your Information Page
+        driver.findElement(By.id("first-name")).sendKeys("Ahmed");
+        driver.findElement(By.id("last-name")).sendKeys("Elkholy");
+        driver.findElement(By.id("postal-code")).sendKeys("112233");
+        driver.findElement(By.id("continue")).click();
+
+        //Finish the Order
+        driver.findElement(By.id("finish")).click();
+        Thread.sleep(2000); //Wait
+
+        //Close Browser
+        driver.close();
+
+
+
+
+
+
+
+
+    }
+}
